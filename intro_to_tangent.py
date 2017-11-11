ffdec8b379793b3fd4d45ee5d11d4621efc32c6e
Brian Gawalt <bgawalt@gmail.com>
2017-11-11 12:02:12 -0800

    Minor cleanup

--- intro_to_tangent.py
+++ intro_to_tangent.py
@@ -1,10 +1,12 @@
 import tangent
 
+
 def cube_x_plus_x(x):
     """f(x) = x^3 + x"""
     a = x * x
     b = a * x
     return b + x
+
 
 def expected_deriv(x):
     """f'(x) = 3*x^2 + 1"""
@@ -16,6 +18,7 @@ def main():
     df = tangent.grad(cube_x_plus_x, verbose=1)
     for i in range(1, 10):
         print i, cube_x_plus_x(i), df(i), expected_deriv(i)
+
 
 if __name__ == "__main__":
     main()
